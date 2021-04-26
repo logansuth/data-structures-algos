b2cdb901db4350f5431014f0a923bdbd3a3895b2
Logan Sutherland <loganrsutherland@gmail.com>
2021-04-26 17:49:57 -0400

    decode: working algorithm

--- decode.js
+++ decode.js
@@ -1,28 +1,34 @@
 /*
-a = 1
-b = 2
-...
-z = 26
+Good morning! Here's your coding interview problem for today.
 
-Count the ways a message can be decoded
+This problem was asked by Facebook.
 
-console.log(decode('111')) // 3
-console.log(decode('2')) // 1
-console.log(decode('79')) // 1
-console.log(decode('26')) // 2
+Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
 
-dynamic programming
+For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
 
-
-
+You can assume that the messages are decodable. For example, '001' is not allowed.
 */
 
 function decode(string) {
-  const stringArr = string.split('');
+  const memo = [1];
 
-  if (stringArr.length === 1) {
-    return sum + 1;
+  function validate(string) {
+    if (string[0] === '1' || (string[0] === '2' && string[1] < 7)) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
-  return sum;
+  for (let i = 1; i < string.length; i++) {
+    memo[i] = memo[i - 1] + validate(string.slice(i - 1, i + 1));
+  }
+
+  return memo[string.length - 1];
 }
+
+console.log(decode('111')); // 3
+console.log(decode('2')); // 1
+console.log(decode('79')); // 1
+console.log(decode('26')); // 2

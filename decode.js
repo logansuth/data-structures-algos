0f54abebfc43381f7aacccf772f5279e2ecb6a63
Logan Sutherland <loganrsutherland@gmail.com>
2021-04-27 11:46:04 -0400

    refactor: update solution for edge case of zero

--- decode.js
+++ decode.js
@@ -8,13 +8,18 @@ Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the nu
 For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
 
 You can assume that the messages are decodable. For example, '001' is not allowed.
+
+Leetcode #91
 */
 
 function decode(string) {
   const memo = [1];
 
   function validate(string) {
-    if (string[0] === '1' || (string[0] === '2' && string[1] < 7)) {
+    if (
+      (string[0] === '1' && string[1] !== '0') ||
+      (string[0] === '2' && string[1] < 7)
+    ) {
       return 1;
     } else {
       return 0;
@@ -22,7 +27,7 @@ function decode(string) {
   }
 
   for (let i = 1; i < string.length; i++) {
-    memo[i] = memo[i - 1] + validate(string.slice(i - 1, i + 1));
+    memo[i] = memo[i - 1] + validate(string[i - 1] + string[i]);
   }
 
   return memo[string.length - 1];
@@ -33,3 +38,4 @@ console.log(decode('2')); // 1
 console.log(decode('79')); // 1
 console.log(decode('26')); // 2
 console.log(decode('11133')); // 4
+console.log(decode('1024')); // 2

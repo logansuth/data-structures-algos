8256681c7f318c5c6112622bde8e63016a120d4d
Logan Sutherland <loganrsutherland@gmail.com>
2021-05-08 19:14:54 -0400

    refactor: fully functional answer that passes on Leetcode

--- decode.js
+++ decode.js
@@ -7,35 +7,34 @@ Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the nu
 
 For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
 
-You can assume that the messages are decodable. For example, '001' is not allowed.
-
 Leetcode #91
 */
 
-function decode(string) {
-  const memo = [1];
+function numDecodings(str) {
+  const length = str.length;
+  const memo = new Array(length + 1).fill(0);
 
-  function validate(string) {
-    if (
-      (string[0] === '1' && string[1] !== '0') ||
-      (string[0] === '2' && string[1] < 7)
-    ) {
-      return 1;
+  memo[length] = 1;
+  memo[length - 1] = str[length - 1] == 0 ? 0 : 1;
+
+  for (let i = length - 2; i > -1; i--) {
+    if (str[i] == 0) {
+      continue;
+    }
+
+    if (str[i] == 1) {
+      memo[i] = memo[i + 1] + memo[i + 2];
+    } else if (str[i] == 2) {
+      memo[i] = memo[i + 1];
+      if (str[i + 1] < 7) {
+        memo[i] += memo[i + 2];
+      }
     } else {
-      return 0;
+      memo[i] = memo[i + 1];
     }
   }
 
-  for (let i = 1; i < string.length; i++) {
-    memo[i] = memo[i - 1] + validate(string[i - 1] + string[i]);
-  }
-
-  return memo[string.length - 1];
+  return memo[0];
 }
 
-console.log(decode('111')); // 3
-console.log(decode('2')); // 1
-console.log(decode('79')); // 1
-console.log(decode('26')); // 2
-console.log(decode('11133')); // 4
-console.log(decode('1024')); // 2
+console.log(numDecodings('12345'));
